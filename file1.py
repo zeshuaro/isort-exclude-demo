1d264f84f9f47f163ccedb050a69ba81dc660d02
Joshua Tang <joshuaystang@gmail.com>
2022-04-27 18:35:17 +1000

    Initial commit

---
+++ file1.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+
+def main():
+    print("Hello from file1")
+
+
+if __name__ == "__main__":
+    main()
---
+++ file2.py
@@ -0,0 +1,11 @@
+import sys
+
+import os
+
+
+def main():
+    print("Hello from file2")
+
+
+if __name__ == "__main__":
+    main()
